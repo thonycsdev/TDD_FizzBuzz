6219963bf8850554d51c84362758850785cc8e23
Anthony Coutinho <thonycout@gmail.com>
2023-12-28 02:24:40 +0000

    TDD FizzBuzz

---
+++ src/__test__/index.spec.ts
@@ -0,0 +1,36 @@
+import { FizzBuzz } from "../fizzbuzz/fizzbuzz";
+
+describe("FizzBuzz", () => {
+    it("Create a FuzzBuzz Object", () => {
+        var fizzBuzz = new FizzBuzz();
+        expect(fizzBuzz).toBeDefined();
+        expect(fizzBuzz.numbers).toHaveLength(100);
+    });
+
+    it("Should return fizz when a number is divisible by 3 and null is it doesnt", () => {
+        var fizzBuzz = new FizzBuzz();
+        const result = fizzBuzz.checkFizzBuzz(3);
+        expect(result).toEqual("Fizz");
+        const result2 = fizzBuzz.checkFizzBuzz(1);
+        expect(result2).toBeNull();
+    });
+
+    it("Should return Buzz when a number is divible by 5", () => {
+        var fizzBuzz = new FizzBuzz();
+        const result = fizzBuzz.checkFizzBuzz(5);
+        expect(result).toEqual("Buzz");
+    });
+    it("Should return FizzBuzz when a number is divible by 5 and 3", () => {
+        var fizzBuzz = new FizzBuzz();
+        const result = fizzBuzz.checkFizzBuzz(15);
+        expect(result).toEqual("FizzBuzz");
+    });
+
+    it("Should check fizzbuzz for all numbers", () => {
+        var fizzBuzz = new FizzBuzz();
+        const spy = jest.spyOn(console, 'log');
+        fizzBuzz.checkFizzBuzzForAllNumbers();
+        expect(spy).toHaveBeenCalledTimes(100);
+    });
+
+});
---
+++ src/fizzbuzz/fizzbuzz.ts
@@ -0,0 +1,49 @@
+export class FizzBuzz {
+    public readonly numbers: number[];
+    constructor() {
+        this.numbers = this.createNumbers();
+    }
+
+    public checkFizzBuzz(number: number): string | null {
+        const isDivisableBy3 = this.checkIfANumberIsDividedByOther(number, 3);
+        const isDivisableBy5 = this.checkIfANumberIsDividedByOther(number,5);
+        const isDivisableBy3And5 = isDivisableBy3 && isDivisableBy5;
+
+        if(isDivisableBy3And5){
+            return "FizzBuzz";
+        }
+        if(isDivisableBy3){
+            return "Fizz";
+        }
+        if(isDivisableBy5){
+            return "Buzz";
+        }
+        return null;
+    }
+
+    private createNumbers(): number[]{
+        var numbers: number[] = [];
+        for (var i = 1; i <= 100; i++) {
+            numbers.push(i);
+        }
+        return numbers;
+    }
+
+    public printNumbers():void {
+        this.numbers.forEach(number => {
+            console.log(`${number}`);
+        });
+    }
+
+
+    public checkFizzBuzzForAllNumbers(): void {
+        this.numbers.forEach((number) => {
+            const result =  this.checkFizzBuzz(number);
+            console.log(`${number} ${result}`);
+        });
+    }
+
+    private checkIfANumberIsDividedByOther(number: number, dividedBy: number): boolean {
+        return number % dividedBy === 0;
+    }
+}
